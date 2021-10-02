ebaf6400b2e74985e711cef3b264144016e5f83b
errr4567 <ega108242424@gmail.com>
2021-10-02 17:55:29 +0300

    None

---
+++ TestAPI2/TestAPI2/Contexts/NoteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPI2.Models;
+
+namespace TestAPI2.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
---
+++ TestAPI2/TestAPI2/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI2.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime CreteDate { get; set; }
+
+        public DateTime? ChangeDate { get; set; }
+    }
+}
